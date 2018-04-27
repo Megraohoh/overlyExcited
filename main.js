adb6296c3cd2797f3cc4684e5bf83fb47aa41245
Meghan Debity <mdebity@gmail.com>
2018-04-27 14:23:08 -0500

    Completed adding '!' to every third word

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-// Create a single function 'addExcitement' that will use console.log() to print out a sentence to the browser console.
-function addExcitement() {
-    console.log()
-} 
+// Create an array that contains the words in the sentence
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+function addExcitement (theWordArray) {
+    //for loop should add one more word to buildMeUp per iteration
+    let buildMeUp = ""
+
+    for (let i = 0; i < theWordArray.length; i++) {
+        // Concatenate the new word onto buildMeUp
+        if ((i + 1) % 3 === 0) {
+            buildMeUp += theWordArray[i] + '!' + ' '
+        } else {
+            buildMeUp += theWordArray[i] + ' '
+        }
+        // Print buildMeUp to the console
+        console.log(buildMeUp)
+    }
+
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence)
